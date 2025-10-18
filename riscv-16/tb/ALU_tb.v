bc2973ca83e66672e342a0581aabc731e46908cf
gjohny <githinjohny@gmail.com>
2025-10-18 15:34:26 -0500

    edit tb to output .vd in correct directory

--- riscv-16/tb/ALU_tb.v
+++ riscv-16/tb/ALU_tb.v
@@ -25,7 +25,7 @@ module ALU_tb;
 
     // For waveform generation
     initial begin
-        $dumpfile("./waveform/ALU_tb.vcd");
+        $dumpfile("./waveform/ALU.vcd");
         $dumpvars(0, ALU_tb);
     end
 
